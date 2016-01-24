8138cd2ae31ef6d58e3a645d623b332b46776a36
Hei Yo <Hei_Yo@bitbucket.org>
2016-01-24 23:59:08 +0300

    AM-360 Retern fields

--- AssetManagement/AssetManager.Infrastructure/Services/ExportService.cs
+++ AssetManagement/AssetManager.Infrastructure/Services/ExportService.cs
@@ -1,43 +1,26 @@
 ﻿using AppFramework.Core.ConstantsEnumerators;
 using AppFramework.Entities;
+using AssetManager.Infrastructure;
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Web;
 using System.Xml.Linq;
-using AppFramework.Core.Classes;
-using AppFramework.Core.Exceptions;
-using Common.Logging;
 
 namespace AssetManager.Infrastructure.Services
 {
     public class ExportService : IExportService
     {
         private readonly IEnvironmentSettings _env;
-        private readonly IUserService _userService;
-        private readonly IAssetsService _assetsService;
-        private readonly IAssetTypeRepository _assetTypeRepository;
-        private readonly ILog _logger;
 
-        public ExportService(IEnvironmentSettings envSettings, IAssetTypeRepository assetTypeRepository = null, IAssetsService assetsService = null, ILog logger = null, IUserService userService = null)
+        public ExportService(IEnvironmentSettings envSettings)
         {
             if (envSettings == null)
                 throw new ArgumentNullException("envSettings");
             _env = envSettings;
-            if (assetTypeRepository == null)
-                throw new ArgumentNullException("assetTypeRepository");
-            _assetTypeRepository = assetTypeRepository;
-            if (assetsService == null)
-                throw new ArgumentNullException("assetsService");
-            _assetsService = assetsService;
-            if (logger == null)
-                throw new ArgumentNullException("logger");
-            _logger = logger;
-            if (userService == null)
-                throw new ArgumentNullException("userService");
-            _userService = userService;
         }
 
         public string ExportSearchResultToTxt(IEnumerable<IIndexEntity> searchResults)
@@ -85,64 +68,18 @@ namespace AssetManager.Infrastructure.Services
         public byte[] ExportSearchResultToExcel(IEnumerable<IIndexEntity> searchResults)
         {
             var dataTable = new DataTable();
-            dataTable.Columns.Add("DynEntityUid", typeof(string));
-            dataTable.Columns.Add("DynEntityId", typeof(string));
-            dataTable.Columns.Add("ActiveVersion", typeof(string));
-            dataTable.Columns.Add("DynEntityConfigUid", typeof(string));
             dataTable.Columns.Add("Name", typeof(string));
-            dataTable.Columns.Add("Revision", typeof(string));
-            dataTable.Columns.Add("Barcode", typeof(string));
-            dataTable.Columns.Add("Update Date", typeof(string));
-            dataTable.Columns.Add("Stock Count", typeof(string));
-            dataTable.Columns.Add("Stock Price", typeof(string));
-            dataTable.Columns.Add("Document", typeof(string));
-            dataTable.Columns.Add("CellenServiceSupport", typeof(string));
-            dataTable.Columns.Add("Location", typeof(string));
-            dataTable.Columns.Add("Base Location", typeof(string));
-            dataTable.Columns.Add("Next Location", typeof(string));
-            dataTable.Columns.Add("Department", typeof(string));
-            dataTable.Columns.Add("User", typeof(string));
-            dataTable.Columns.Add("Owner", typeof(string));
-            dataTable.Columns.Add("Update User", typeof(string));
-
+            dataTable.Columns.Add("CommonInfo", typeof(string));
+            dataTable.Columns.Add("Details", typeof(string));
+            dataTable.Columns.Add("DirectUrl", typeof(string));
 
             foreach (var item in searchResults)
             {
-                var assetType = _assetTypeRepository.GetById(item.DynEntityConfigId);
-                var asset = _assetsService.GetAssetById(item.DynEntityId, assetType);
-
-                var activeVersion = asset.Attributes.Find(attr => attr.Configuration.Name == "ActiveVersion");
-                var revision = asset.Attributes.Find(attr => attr.Configuration.Name == "Revision");
-                var stockCount = asset.Attributes.Find(attr => attr.Configuration.Name == "Stock Count");
-                var stockPrice = asset.Attributes.Find(attr => attr.Configuration.Name == "Stock Price");
-                var document = asset.Attributes.Find(attr => attr.Configuration.Name == "Document");
-                var cellenServiceSupport = asset.Attributes.Find(attr => attr.Configuration.Name == "CellenServiceSupport");
-                var baseLocation = asset.Attributes.Find(attr => attr.Configuration.Name == "Base Location");
-                var nextLocation = asset.Attributes.Find(attr => attr.Configuration.Name == "Next Location");
-                var user = asset.Attributes.Find(attr => attr.Configuration.Name == "User");
-                var owner = asset.Attributes.Find(attr => attr.Configuration.Name == "Owner");
-                var updateUser = asset.Attributes.Find(attr => attr.Configuration.Name == "Update User");
-                
                 dataTable.Rows.Add(
-                    item.DynEntityUid,
-                    item.DynEntityId,
-                    activeVersion != null ? activeVersion.Value: string.Empty,
-                    item.DynEntityConfigUid,
-                    revision != null ? revision.Value : string.Empty,
                     item.Name,
-                    item.BarCode,
-                    item.UpdateDate,
-                    stockCount != null ? stockCount.Value : string.Empty,
-                    stockPrice != null ? stockPrice.Value : string.Empty,
-                    document != null ? document.Value : string.Empty,
-                    "CellenServiceSupport",
-                    item.Location,
-                    baseLocation != null ? baseLocation.Value : string.Empty,
-                    nextLocation != null ? nextLocation.Value : string.Empty,
-                    item.Department,
-                    user != null ? user.Value : string.Empty,
-                    owner != null ? owner.Value : string.Empty,
-                    updateUser != null ? updateUser.Value : string.Empty);
+                    item.Subtitle,
+                    item.DisplayExtValues,
+                    _env.GetPathToAssetPage(item.DynEntityConfigId, item.DynEntityId));
             }
 
             using (ExcelPackage pck = new ExcelPackage())
